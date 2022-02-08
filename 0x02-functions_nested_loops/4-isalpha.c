4a9456ec8e6f096ecc67c0f8a81655111dec21a1
KristiSeraj <kristiseraj2@gmail.com>
2022-02-08 05:15:20 -0800

    Comment for function, argument, return type fixed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - checks if character is lowercase or uppercase
- * @c - the character to be checked if it is lowercase or uppercase
+ * @c: the character to be checked if it is lowercase or uppercase
  * Return: 1 if c is uppercase or lowercase and 0 if not
  */
 int _isalpha(int c)
