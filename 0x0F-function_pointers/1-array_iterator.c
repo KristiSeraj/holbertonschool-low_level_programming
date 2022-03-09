dbe0d0b990d5492f881ebffc8b29f197f67a2d5e
KristiSeraj <kristiseraj2@gmail.com>
2022-03-09 02:50:32 -0800

    Betty style fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,15 @@
 #include "function_pointers.h"
-
+/**
+ * array_iterator - function that executes a function given as a paramter
+ * on each element of an array
+ * @array: array of elements
+ * @size: size of the array
+ * @action: a pointer to the function
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t k;
-	
+
 	k = 0;
 	while (k < size)
 	{
