da19ff2c035b0f88ec0c5934f0ad4ee2ce796c05
KristiSeraj <kristiseraj2@gmail.com>
2022-03-10 05:18:59 -0800

    Betty style fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
-
+/**
+ * print_strings - function that prints strings
+ * @separator: the string to be printed between the strings
+ * @n: number of args
+ * @...: args
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ags;
