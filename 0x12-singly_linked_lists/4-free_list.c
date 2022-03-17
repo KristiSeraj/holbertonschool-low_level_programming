4a62ea122d4bb106389c2d1d05725f20ff880718
KristiSeraj <kristiseraj2@gmail.com>
2022-03-17 05:50:18 -0700

    Code in C that frees a list_t list fixed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,8 +8,10 @@ void free_list(list_t *head)
 {
 	list_t *ptr;
 
-	for (; head != NULL; ptr = head, head = head->next)
+	while (head != NULL)
 	{
+		ptr = head;
+		head = head->next;
 		free(ptr->str);
 		free(ptr);
 	}
