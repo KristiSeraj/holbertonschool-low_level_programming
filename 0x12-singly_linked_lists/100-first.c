dcd623d0b0bb7ae7242257d99fdb8c0473a09b99
KristiSeraj <kristiseraj2@gmail.com>
2022-03-18 02:35:36 -0700

    Betty style fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-void first (void) __attribute__ ((constructor));
-
-void first (void)
+void first(void) __attribute__ ((constructor));
+/**
+ * first - function that executes before main function
+ */
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
