e92b6984fce532e97134a59a1c515f0b8b685ac1
KristiSeraj <kristiseraj2@gmail.com>
2022-03-23 02:21:37 -0700

    Code in C to insert a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	while (i < (idx - 1))
 	{
+		if (ptr == NULL || ptr->next)
+			return (NULL);
 		ptr = ptr->next;
 		i++;
 	}
