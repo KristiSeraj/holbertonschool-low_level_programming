764b5194e1c51c616b92defadde09707e4d50fa4
KristiSeraj <kristiseraj2@gmail.com>
2022-03-25 02:11:01 -0700

    Betty style fix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * get_endianness - check for endianness
+ * Return: o if big endian, 1 if little endian
+ */
 int get_endianness(void)
 {
 	unsigned int x = 1;
-	char *c = (char*) &x;
-	return (int)*c;
+	char *c = (char *) &x;
+
+	return ((int)*c);
 }
