852b22eabda49a94f673663b72cd5f0134b546c0
KristiSeraj <kristiseraj2@gmail.com>
2022-03-25 01:21:50 -0700

    Fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	while (result_xor)
 	{
 		if (result_xor & 1)
-			count_bits += (result_xor & 1);
+			count_bits++;
 		result_xor >>= 1;
 	}
 	return (count_bits);
