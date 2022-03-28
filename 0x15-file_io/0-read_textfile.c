e44b060a7c5c45c1747413e7222db2681bd3772a
KristiSeraj <kristiseraj2@gmail.com>
2022-03-28 03:08:26 -0700

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,13 +16,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
 	text = malloc(sizeof(char) * letters);
 	if (text == NULL)
 		return (0);
 	read_f = read(file, text, letters);
+	if (read_f == -1)
+		return (0);
 	write_f = write(STDIN_FILENO, text, read_f);
-
-	if (file == -1 || read_f == -1 || write_f == -1)
+	if (write_f == -1)
 		return (0);
 
 	free(text);
