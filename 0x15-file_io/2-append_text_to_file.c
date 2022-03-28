b7c07d3928297f501ac5c1c6dc37bd52e3344393
KristiSeraj <kristiseraj2@gmail.com>
2022-03-28 06:38:39 -0700

    Fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,14 +9,14 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, write_f, len;
 
+	if (filename == NULL)
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
 			;
 	}
-	if (filename == NULL)
-		return (-1);
-	file = open(filename, O_WRONLY | O_APPEND, 0200);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 	write_f = write(file, text_content, len);
