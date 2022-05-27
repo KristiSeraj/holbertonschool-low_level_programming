96999d49352dd17db4126922d4ac9da8af2f852b
KristiSeraj <kristiseraj2@gmail.com>
2022-05-27 01:29:31 -0700

    new dir added

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,8 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
