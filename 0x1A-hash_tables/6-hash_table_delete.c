2b1c632cad4b50aa79df961429e7021f7d4f42d3
KristiSeraj <kristiseraj2@gmail.com>
2022-05-27 03:50:26 -0700

    fix

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,12 +5,12 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp;
+	hash_node_t *temp;
 	unsigned long int i;
 
 	if (ht == NULL)
 		return;
-	for (i = 0; i < ht->size; ++i)
+	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
